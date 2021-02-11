368800409521cdb237b3c6e3a1dd5eb28cc6bb48
Riskyo <68279330+Riskyo@users.noreply.github.com>
2021-02-11 07:18:40 +0700

    minggu 6

--- react-js/src/pages/Menu.js
+++ react-js/src/pages/Menu.js
@@ -6,17 +6,18 @@ function Menu() {
     const [menus,setMenu]=useState(
         [
             {idmenu:1, idkategori:1, menu:"Apel Manalagi", gambar:"3.jpg", harga:6000},
-            {idmenu:2, idkategori:2, menu:"Pisang Raja", gambar:"4.jpg", harga:8500},
-            {idmenu:3, idkategori:3, menu:"Nasi Padang", gambar:"5.jpg", harga:4500},
-            {idmenu:4, idkategori:4, menu:"Nasi Ayam", gambar:"6.jpg", harga:1500},
-            {idmenu:5, idkategori:5, menu:"Es Teh", gambar:"1.jpg", harga:1500},
-            {idmenu:6, idkategori:6, menu:"Es Teh", gambar:"2.jpg", harga:1500},
+            {idmenu:2, idkategori:1, menu:"Pisang Raja", gambar:"4.jpg", harga:8500},
+            {idmenu:3, idkategori:2, menu:"Nasi Padang", gambar:"5.jpg", harga:4500},
+            {idmenu:4, idkategori:2, menu:"Nasi Ayam", gambar:"6.jpg", harga:1500},
+            {idmenu:5, idkategori:3, menu:"Es Teh", gambar:"1.jpg", harga:1500},
+            {idmenu:6, idkategori:3, menu:"Es Teh", gambar:"2.jpg", harga:1500},
         ]
     );
 
     return (
       <div className="App">
         <Tabel menu = {menus} titel = {titel}/>
+        <Tabel menu = {menus.filter((data)=>(data.idkategori===3))} titel = "Menu Buah"/>
       </div>
     );
   }
